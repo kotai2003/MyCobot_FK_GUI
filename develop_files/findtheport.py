a1842eb3bb023e71154aa589ca1c52efa1998229
S Choe <seonghun.choe@tomomi-research.com>
2021-03-31 15:24:38 +0900

    Delete develop_files directory

--- develop_files/findtheport.py
+++
@@ -1,5 +0,0 @@
-import serial.tools.list_ports
-
-port_list = list(serial.tools.list_ports.comports())
-for item in port_list:
-    print(item[0])
--- develop_files/test_set_Free_mode.py
+++
@@ -1,8 +0,0 @@
-from pymycobot.mycobot import MyCobot
-from pymycobot.genre import Coord, Angle, Coord
-import time
-
-speed = 20
-mycobot = MyCobot('COM6')
-
-mycobot.set_free_mode()
